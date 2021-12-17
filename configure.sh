9a9acea230d046c3de9367c1188c6d23b94b7d73
adefilippi <anthony.defilippi@gmail.com>
2021-12-17 21:19:38 +0100

    fix exts

--- configure.sh
+++ configure.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
+echo  ${PHPIZE_DEPS}
 set -ex
 
-PHP_EXTENSIONS="bcmath bz2 calendar ctype curl dba dom enchant exif ffi fileinfo filter ftp gd gettext gmp hash iconv imap intl json ldap mbstring mysqli oci8 odbc opcache pcntl pdo pdo_dblib pdo_firebird pdo_mysql pdo_oci pdo_odbc pdo_pgsql pdo_sqlite pgsql phar posix pspell readline reflection session shmop simplexml snmp soap sockets sodium spl standard sysvmsg sysvsem sysvshm tidy tokenizer xml xmlreader xmlrpc xmlwriter xsl zend_test zip"
+PHP_EXTENSIONS="opcache bcmath bz2 calendar exif gd gettext gmp json intl mcrypt mysqli recode pdo_mysql pdo_oci pdo_odbc pdo_pgsql pdo_sqlite pgsql shmop soap sockets sysvmsg sysvsem sysvshm tidy xsl mysqli pdo_mysql wddx zip pcntl"
 PECL_EXTENSIONS="apcu imagick sqlsrv pdo_sqlsrv mcrypt"
 RUN_DEPS="unzip libzip icu libxslt imagemagick libmcrypt recode tidyhtml freetype libjpeg-turbo libpng libwebp libxpm make"
 BUILD_DEPS="autoconf g++ libzip-dev zlib-dev libpng-dev libxml2-dev icu-dev bzip2-dev libc-dev gmp-dev libmcrypt-dev recode-dev gettext-dev tidyhtml-dev libxslt-dev imagemagick-dev freetype-dev libjpeg-turbo-dev libpng-dev libwebp-dev libxpm-dev tzdata unixodbc-dev"
 
-apk add --no-cache --virtual rundeps ${RUN_DEPS}
-apk add --no-cache --virtual .build-deps ${BUILD_DEPS}
 apk update
 
 apk add --no-cache fcgi file gettext gnu-libiconv bash
@@ -21,7 +20,7 @@ apk add --no-cache --virtual .build-deps ${BUILD_DEPS}
 
 
 docker-php-source extract
-docker-php-ext-configure gd --with-freetype --with-jpeg --with-png --with-xpm
+docker-php-ext-configure gd --with-freetype --with-jpeg
 docker-php-ext-install -j"$(nproc)" ${PHP_EXTENSIONS}
 pecl install ${PECL_EXTENSIONS}
 docker-php-ext-enable ${PECL_EXTENSIONS}
