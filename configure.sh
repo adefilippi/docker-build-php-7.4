30cd5d7b908bda59e0df175e6a904a075f53870b
adefilippi <adefilippi@agencedps.com>
2022-01-14 23:06:29 +0100

    Develop (#4)
    * add odbc driver
    
    * add odbc
    
    * Update configure.sh

--- configure.sh
+++ configure.sh
@@ -25,10 +25,6 @@ apk add unixodbc-dev
 curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/msodbcsql17_17.6.1.1-1_amd64.apk
 curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/mssql-tools_17.6.1.1-1_amd64.apk
 
-curl https://packages.microsoft.com/keys/microsoft.asc  | gpg --import -
-gpg --verify msodbcsql17_17.6.1.1-1_amd64.sig msodbcsql17_17.6.1.1-1_amd64.apk
-gpg --verify mssql-tools_17.6.1.1-1_amd64.sig mssql-tools_17.6.1.1-1_amd64.apk
-
 apk add --allow-untrusted msodbcsql17_17.6.1.1-1_amd64.apk
 apk add --allow-untrusted mssql-tools_17.6.1.1-1_amd64.apk
 
