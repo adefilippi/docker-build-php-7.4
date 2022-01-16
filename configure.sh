d505ab1534beb9d5682ec319a034c58387e8dd01
adefilippi <anthony.defilippi@gmail.com>
2022-01-16 20:24:05 +0100

    change iconv version

--- configure.sh
+++ configure.sh
@@ -10,12 +10,13 @@ BUILD_DEPS="autoconf g++ libzip-dev zlib-dev libpng-dev libxml2-dev icu-dev bzip
 
 apk update
 
-apk add --no-cache fcgi file gettext gnu-libiconv bash postgresql-dev
+apk add --no-cache fcgi file gettext  bash postgresql-dev
 
 
 # install gnu-libiconv and set LD_PRELOAD env to make iconv work fully on Alpine image.
 # see https://github.com/docker-library/php/issues/240#issuecomment-763112749
-ENV LD_PRELOAD /usr/lib/preloadable_libiconv.so php
+apk add --upgrade gnu-libiconv=1.15-r2
+export LD_PRELOAD /usr/lib/preloadable_libiconv.so
 
 apk add --no-cache --virtual rundeps ${RUN_DEPS}
 apk add --no-cache --virtual .build-deps ${BUILD_DEPS}
