7f9de9c87b7c479293e6011ae47672b604c5c03b
adefilippi <anthony.defilippi@gmail.com>
2022-01-20 11:54:31 +0100

    iconv

--- configure.sh
+++ configure.sh
@@ -12,8 +12,6 @@ apk update
 
 apk add --no-cache fcgi file gettext bash postgresql-dev
 
-apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/community/ --allow-untrusted gnu-libiconv-dev
-export LD_PRELOAD="/usr/lib/preloadable_libiconv.so"
 
 apk add --no-cache --virtual rundeps ${RUN_DEPS}
 apk add --no-cache --virtual .build-deps ${BUILD_DEPS}
@@ -32,9 +30,6 @@ docker-php-ext-install -j"$(nproc)" ${PHP_EXTENSIONS}
 pecl install ${PECL_EXTENSIONS_PACKAGES}
 docker-php-ext-enable ${PECL_EXTENSIONS}
 
-
-
-
 docker-php-source delete
 rm -r /tmp/pear/cache/* /tmp/pear/download/*
 
@@ -47,3 +42,31 @@ apk del .build-deps
 ### create php-session DIR
 mkdir /tmp/php-sessions/
 chmod +rw /tmp/php-sessions/
+
+
+# Install GNU libiconv
+&& mkdir -p /opt \
+&& cd /opt \
+&& wget http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.15.tar.gz \
+&& tar xzf libiconv-1.15.tar.gz \
+&& cd libiconv-1.15 \
+&& sed -i 's/_GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");/#if HAVE_RAW_DECL_GETS\n_GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");\n#endif/g' srclib/stdio.in.h \
+&& ./configure --prefix=/usr/local \
+&& make \
+&& make install \
+# Install PHP iconv from source
+&& cd /opt \
+&& wget http://php.net/distributions/php-7.1.5.tar.gz \
+&& tar xzf php-7.1.5.tar.gz \
+&& cd php-7.1.5/ext/iconv \
+&& phpize \
+&& ./configure --with-iconv=/usr/local \
+&& make \
+&& make install \
+&& mkdir -p /etc/php7/conf.d \
+&& echo "extension=iconv.so" >> /etc/php7/conf.d/iconv.ini \
+# Cleanup
+&& apk del $BUILD_PACKAGES \
+&& rm -rf /opt \
+&& rm -rf /var/cache/apk/* \
+&& rm -rf /usr/share/*
