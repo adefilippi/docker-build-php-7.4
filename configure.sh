281f631cf76fdd747b47510f9728a91a28f0d3fe
adefilippi <anthony.defilippi@gmail.com>
2022-01-20 11:11:59 +0100

    iconv

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@ echo  ${PHPIZE_DEPS}
 set -ex
 
 PHP_EXTENSIONS="opcache bcmath bz2 calendar exif gd gettext gmp json intl mysqli pdo_mysql pdo_pgsql pgsql shmop soap sockets zip iconv"
-PECL_EXTENSIONS_PACKAGES="apcu imagick sqlsrv pdo_sqlsrv mcrypt"
+PECL_EXTENSIONS_PACKAGES="apcu imagick sqlsrv pdo_sqlsrv mcrypt php7-iconv"
 PECL_EXTENSIONS="apcu imagick sqlsrv pdo_sqlsrv mcrypt"
 RUN_DEPS="unzip libzip icu libxslt imagemagick libmcrypt recode tidyhtml freetype libjpeg-turbo libpng libwebp libxpm make"
 BUILD_DEPS="autoconf g++ libzip-dev zlib-dev libpng-dev libxml2-dev icu-dev bzip2-dev libc-dev gmp-dev libmcrypt-dev recode-dev gettext-dev tidyhtml-dev libxslt-dev imagemagick-dev freetype-dev libjpeg-turbo-dev libpng-dev libwebp-dev libxpm-dev tzdata unixodbc-dev"
