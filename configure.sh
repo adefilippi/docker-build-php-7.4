dba67dc7426e9880406b4078411e1085d82f1f53
adefilippi <anthony.defilippi@gmail.com>
2022-01-21 18:57:44 +0100

    lib iconv

--- configure.sh
+++ configure.sh
@@ -12,7 +12,7 @@ apk update
 
 apk add --no-cache fcgi file gettext bash postgresql-dev
 
-apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/v3.12/community/ --allow-untrusted gnu-libiconv=1.15-r2
+apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/v3.13/community/ --allow-untrusted gnu-libiconv=1.15-r3
 export LD_PRELOAD="/usr/lib/preloadable_libiconv.so"
 
 
@@ -33,35 +33,6 @@ docker-php-ext-configure gd --with-freetype --with-jpeg
 docker-php-ext-install -j"$(nproc)" ${PHP_EXTENSIONS}
 pecl install ${PECL_EXTENSIONS_PACKAGES}
 docker-php-ext-enable ${PECL_EXTENSIONS}
-
-
-# Install GNU libiconv
-mkdir -p /opt \
-&& cd /opt \
-&& wget http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.15.tar.gz \
-&& tar xzf libiconv-1.15.tar.gz \
-&& cd libiconv-1.15 \
-&& sed -i 's/_GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");/#if HAVE_RAW_DECL_GETS\n_GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");\n#endif/g' srclib/stdio.in.h \
-&& ./configure --prefix=/usr/local \
-&& make \
-&& make install \
-# Install PHP iconv from source
-&& cd /opt \
-&& wget http://php.net/distributions/php-7.1.5.tar.gz \
-&& tar xzf php-7.1.5.tar.gz \
-&& cd php-7.1.5/ext/iconv \
-&& phpize \
-&& ./configure --with-iconv=/usr/local \
-&& make \
-&& make install \
-&& mkdir -p /etc/php7/conf.d \
-&& echo "extension=iconv.so" >> /etc/php7/conf.d/iconv.ini \
-# Cleanup
-&& apk del $BUILD_PACKAGES \
-&& rm -rf /opt \
-&& rm -rf /var/cache/apk/* \
-&& rm -rf /usr/share/*
-
 
 docker-php-source delete
 rm -r /tmp/pear/cache/* /tmp/pear/download/*
